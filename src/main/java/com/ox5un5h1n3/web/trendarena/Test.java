9044816766ecc0d29e00756ad62933b2deab61a5
Ox5un5h1n3 <5un5h1n34096@protonmail.com>
2023-08-17 17:47:19 +0530

    Added Hibernate configuration

--- src/main/java/com/ox5un5h1n3/web/trendarena/Test.java
+++ src/main/java/com/ox5un5h1n3/web/trendarena/Test.java
@@ -1,2 +1,9 @@
-package com.ox5un5h1n3.web.trendarena;public class Test {
+package com.ox5un5h1n3.web.trendarena;
+
+import com.ox5un5h1n3.web.trendarena.util.HibernateUtil;
+
+public class Test {
+    public static void main(String[] args) {
+        HibernateUtil.getSessionFactory();
+    }
 }
--- src/main/java/com/ox5un5h1n3/web/trendarena/entity/User.java
+++ src/main/java/com/ox5un5h1n3/web/trendarena/entity/User.java
@@ -1,2 +1,30 @@
-package com.ox5un5h1n3.web.trendarena.entity;public class User {
+package com.ox5un5h1n3.web.trendarena.entity;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+
+@Entity
+@Table(name = "users")
+public class User implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+    private String name;
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
 }
